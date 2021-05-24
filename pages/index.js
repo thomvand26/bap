fa49e6e4d96c9090ff91ee473ed37d049454d85f
thomvand26 <thomvand26@student.arteveldehs.be>
2021-05-24 22:54:01 +0200

    Update homepage

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,56 @@
-import { ShowList } from '../components';
-import { Layouts } from '../layouts';
-// import styles from './index.module.scss';
 import { useSession } from 'next-auth/client';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
+
+import { ShowList } from '@/components';
+import { Layouts } from '@/layouts';
 import { LOGIN } from '@/routes';
+import { useDatabase } from '@/context';
+
+// import styles from './index.module.scss';
 
 export default function HomePage() {
-  const [session] = useSession();
+  const [session, loading] = useSession();
+  const { getShows } = useDatabase();
+  const [playingShows, setPlayingShows] = useState();
   const router = useRouter();
 
   useEffect(() => {
     if (session === undefined) return;
-    if (!session) router.push(LOGIN);
+    if (!session || !session?.user?._id) router.push(LOGIN);
   }, [session]);
+
+  useEffect(() => {
+    if (loading) return;
+
+    const userId = session?.user?._id;
+
+    if (!userId) {
+      console.log('no user id, user not logged in?');
+      return;
+    }
+
+    (async () => {
+      const currentDate = new Date().toISOString();
+
+      const response = await getShows({
+        startDate: {
+          $lt: currentDate,
+        },
+        endDate: {
+          $gt: currentDate,
+        },
+        visible: true,
+      });
+
+      setPlayingShows(response);
+    })();
+  }, [session, loading]);
 
   return (
     <div className="page">
       <h2 className="pageHeader">Shows</h2>
+      <ShowList shows={playingShows} />
     </div>
   );
 }
