30e81ec3b57444371687573eb154a5cc88b06f9b
thomvand26 <thomvand26@student.arteveldehs.be>
2021-06-01 22:53:01 +0200

    Add chat top section

--- components/Chat/Chat.jsx
+++ components/Chat/Chat.jsx
@@ -1,19 +1,22 @@
 import React, { useState } from 'react';
 import { Form, Formik } from 'formik';
 import * as Yup from 'yup';
+import Link from 'next/link';
+import { MdExitToApp } from 'react-icons/md';
 
 import { useShow } from '@/context';
 import { Input } from '@/components';
 import { Chatroom } from './Chatroom';
 
 import styles from './Chat.module.scss';
+import { LANDING } from '@/routes';
 
 const validationSchema = Yup.object().shape({
   message: Yup.string().trim(),
 });
 
 export const Chat = () => {
-  const { sendChat } = useShow();
+  const { sendChat, currentShow } = useShow();
 
   const handleSubmit = (values, actions) => {
     if (values?.message?.length) {
@@ -25,6 +28,17 @@ export const Chat = () => {
 
   return (
     <div className={styles.chatContainer}>
+      <div className={styles.topSection}>
+        {currentShow?.title && (
+          <h2 className={styles.showTitle}>{currentShow.title}</h2>
+        )}
+        <Link href={LANDING}>
+          <MdExitToApp
+            className={`button button--icon button--danger button--hover-light ${styles.actionButton}`}
+            size="1.8rem"
+          />
+        </Link>
+      </div>
       <div className={styles.requests}>Song requests</div>
       <div className={styles.chats}>
         <Chatroom />
@@ -38,11 +52,7 @@ export const Chat = () => {
         onSubmit={handleSubmit}
       >
         <Form className={styles.bottom}>
-          <Input
-            name="message"
-            type="text"
-            autoComplete="off"
-          />
+          <Input name="message" type="text" autoComplete="off" />
           <button type="submit">Send</button>
         </Form>
       </Formik>
