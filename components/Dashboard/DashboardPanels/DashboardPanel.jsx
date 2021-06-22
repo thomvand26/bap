724a11390633a131d158d6b630f6321e84087bf4
thomvand26 <thomvand26@student.arteveldehs.be>
2021-06-22 23:00:38 +0200

    Update DashboardPanel

--- components/Dashboard/DashboardPanels/DashboardPanel.jsx
+++ components/Dashboard/DashboardPanels/DashboardPanel.jsx
@@ -2,13 +2,28 @@ import React from 'react';
 
 import styles from './dashboardPanel.module.scss';
 
-export const DashboardPanel = ({ name, toggle, row = 1, colspan = 1, rowspan = 1, children }) => {
+export const DashboardPanel = ({
+  name,
+  toggle,
+  row = 1,
+  colspan = 1,
+  rowspan = 1,
+  children,
+}) => {
   return (
     <div
       className={`${styles.panel} ${styles[`row--${row}`]}`}
       style={{ gridColumn: `span ${colspan}`, gridRow: `span ${rowspan}` }}
     >
-      {name && <div className={`${styles.panel__name} ${toggle ? styles['panel__name--toggle'] : ''}`}>{name}</div>}
+      {name && (
+        <div
+          className={`${styles.panel__name} ${
+            toggle ? styles['panel__name--toggle'] : ''
+          }`}
+        >
+          {name}
+        </div>
+      )}
       <div className={styles.panel__content}>{children}</div>
     </div>
   );
