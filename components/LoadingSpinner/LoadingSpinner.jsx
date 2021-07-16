6d3eed5e056bcb6f247d960ce77ef8364f708edc
thomvand26 <thomvand26@student.arteveldehs.be>
2021-07-16 23:22:08 +0200

    Update LoadingSpinner

--- components/LoadingSpinner/LoadingSpinner.jsx
+++ components/LoadingSpinner/LoadingSpinner.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
 
-export const LoadingSpinner = () => {
-  return <div>Loading...</div>;
+import styles from './LoadingSpinner.module.scss';
+
+export const LoadingSpinner = ({ message = 'Loading', horizontal, size }) => {
+  return (
+    <div
+      className={`${styles.container} ${
+        horizontal ? styles['container--horizontal'] : ''
+      } ${size  ? styles[`container--${size}`] : ''}`}
+    >
+      <div className={`${styles.shapeshifter}`}></div>
+      {message && <div className={styles.message}>{message}</div>}
+    </div>
+  );
 };
