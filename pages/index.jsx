a0e143bdc83768bc3162bf988d1ef6680e6cd4fd
thomvand26 <thomvand26@student.arteveldehs.be>
2021-07-17 18:05:55 +0200

    Update homepage

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,13 +2,13 @@ import { useSession } from 'next-auth/client';
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 
-import { ShowList } from '@/components';
+import { ShowList, LoadingSpinner } from '@/components';
 import { Layouts } from '@/layouts';
 import { LOGIN } from '@/routes';
 import { useDatabase, useShow } from '@/context';
 import { upsertDocumentInArrayState, filterShowsPlayingNow } from '@/utils';
 
-// import styles from './index.module.scss';
+import styles from './index.module.scss';
 
 export default function HomePage() {
   const [session, loading] = useSession();
@@ -39,9 +39,6 @@ export default function HomePage() {
         startDate: {
           $lt: currentDate,
         },
-        endDate: {
-          $gt: currentDate,
-        },
         visible: true,
       });
 
@@ -53,14 +50,30 @@ export default function HomePage() {
   }, [session, loading]);
 
   return (
-    <div className="page">
-      <h2 className="pageHeader">Shows</h2>
-      <ShowList
-        shows={filterShowsPlayingNow({
-          shows: fetchedShows,
-          onlyVisible: true,
-        })}
-      />
+    <div className="page container">
+      {/* <h2 className="pageHeader">Shows</h2> */}
+      <section className={`container__content ${styles.section}`}>
+        <ShowList
+          headers={['Currently playing']}
+          shows={filterShowsPlayingNow({
+            shows: fetchedShows,
+            onlyVisible: true,
+          }).slice(0, 3)}
+          cards
+          isOnHome
+          withBrowseAllButton
+        />
+      </section>
+      <section className={`container__content ${styles.section}`}>
+        <ShowList
+          headers={['Upcoming shows']}
+          // TODO: NOT now playing
+          shows={fetchedShows.filter((show) => show?.visible).slice(0, 3)}
+          isOnHome
+          withBrowseAllButton
+          variant="upcoming"
+        />
+      </section>
     </div>
   );
 }
