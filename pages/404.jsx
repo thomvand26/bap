860eac9ed0c0e3165a910638913fded15f37bb36
thomvand26 <thomvand26@student.arteveldehs.be>
2021-07-21 20:46:52 +0200

    Add 404 page

---
+++ pages/404.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Link from 'next/link';
+
+import { Layouts } from '@/layouts';
+import { LANDING } from '@/routes';
+
+import styles from './index.module.scss';
+
+export default function Custom404() {
+  return (
+    <div className={`page ${styles.page404}`}>
+      <h1 className={styles.heading}>404 - Pagina niet gevonden</h1>
+      <div className={styles.info}>
+        De pagina die je zoekt is gewijzigd of bestaat niet meer.
+      </div>
+      <Link type="button" href={LANDING}>
+        <a className="button button--fit">Ga terug naar de startpagina</a>
+      </Link>
+    </div>
+  );
+}
+
+Custom404.layout = Layouts.default;
