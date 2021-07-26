a387e48f284757f8c7fbc92e45428482185a5cfa
thomvand26 <thomvand26@student.arteveldehs.be>
2021-07-26 22:06:34 +0200

    Fix useWindowSize issue

--- utils/hooks/useWindowSize.js
+++ utils/hooks/useWindowSize.js
@@ -1,20 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export const useWindowSize = () => {
-  const [windowSize, setWindowSize] = React.useState({
-    width: window.innerWidth,
-    height: window.innerHeight,
+  const [windowSize, setWindowSize] = useState({
+    width: (process.browser && window?.innerWidth) || 1600,
+    height: (process.browser && window?.innerHeight) || 900,
   });
 
-  React.useEffect(() => {
+  useEffect(() => {
+    if (!process.browser || !window) return;
+
     const updateWindowSize = () => {
-      setWindowSize({ width: window.innerWidth, height: window.innerHeight });
+      setWindowSize({ width: window?.innerWidth, height: window?.innerHeight });
     };
 
-    window.addEventListener('resize', updateWindowSize);
+    window?.addEventListener('resize', updateWindowSize);
 
     return () => {
-      window.removeEventListener('resize', updateWindowSize);
+      if (process.browser && window) {
+        window?.removeEventListener('resize', updateWindowSize);
+      }
     };
   }, []);
 
