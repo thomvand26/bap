3d7a2285ca6bd1dcdf32e4fdc138bc4223feae4a
thomvand26 <thomvand26@student.arteveldehs.be>
2021-07-29 19:09:23 +0200

    Add favicon

--- layouts/DefaultLayout.jsx
+++ layouts/DefaultLayout.jsx
@@ -10,7 +10,36 @@ export const DefaultLayout = ({ children }) => {
     <div className={styles.page}>
       <Head>
         <title>{appConfig.appName}</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          rel="mask-icon"
+          href="/favicon/safari-pinned-tab.svg"
+          color="#ff7700"
+        />
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#ff7700" />
+        <meta
+          name="msapplication-config"
+          content="/favicon/browserconfig.xml"
+        />
+        <meta name="theme-color" content="#ff7700" />
       </Head>
 
       <Header />
--- layouts/ShowLayout.jsx
+++ layouts/ShowLayout.jsx
@@ -9,7 +9,36 @@ export const ShowLayout = ({ children }) => {
     <div className={styles.page}>
       <Head>
         <title>ROOM {appConfig.appName}</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          rel="mask-icon"
+          href="/favicon/safari-pinned-tab.svg"
+          color="#ff7700"
+        />
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#ff7700" />
+        <meta
+          name="msapplication-config"
+          content="/favicon/browserconfig.xml"
+        />
+        <meta name="theme-color" content="#ff7700" />
       </Head>
 
       <main className={styles.main}>{children}</main>
